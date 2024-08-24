aa2d88e1ae1a466c495a741edff0d33d3cc39a0d
Cademon Bishop <cademon101@gmail.com>
2024-08-24 10:16:39 -0400

    Hoping this is my first commit

---
+++ LeetCodeTesting/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LeetCodeSoultions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Test Commit This is Working");
+        
+        Console.ReadLine();
+    }
+}
